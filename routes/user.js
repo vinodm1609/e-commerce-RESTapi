e2af9d8fcc8132b184bd9ea35ac23a835c9e5277
vinod mishra <mvinod2783@gamil.com>
2022-10-19 11:19:54 +0530

    refer token for rgister

--- routes/user.js
+++ routes/user.js
@@ -4,11 +4,13 @@ import registerController from "../controller/auth/user_registers.js";
 import loginController from "../controller/auth/loginController.js";
 import userController from '../controller/auth/userController.js'
 import auth from "../middleware/auth.js";
+import referTokenCon from "../controller/auth/referring.js"
 
 // user ko register kar ka liya 
 // controller ma auth folder ka andre  logic likha  kar ka route ma reference diya hai
 router.post('/register', registerController.register);
 router.post('/login', loginController.login);
 router.get('/me', auth, userController.me);
+router.post('/referToken', referTokenCon.refer)
 
 export default router; 
